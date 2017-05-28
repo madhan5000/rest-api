f691eff0da7c2f18a7a192187640bd8a3211c886
Madhan Sundaram <msundaram@scrippsnetworks.com>
2017-05-28 00:23:18 -0400

    openshift port

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 	    name: 'rest-api',
 	    version: '0.0.1',
 	    env: process.env.NODE_ENV || 'development',
-	    port: process.env.PORT || 3000,
+	    port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 	    db: {
 	        uri: process.env.MONGO_URL
 			//uri : 'mongodb://localhost:27017/api'
