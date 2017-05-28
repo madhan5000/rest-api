fc3bf52b0d8d8023e54f19fe4eb825c9eb5c50c2
Madhan Sundaram <madhan5000@users.noreply.github.com>
2017-05-27 23:44:50 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+	module.exports = {
+	    name: 'rest-api',
+	    version: '0.0.1',
+	    env: process.env.NODE_ENV || 'development',
+	    port: process.env.PORT || 3000,
+	    db: {
+	        uri: process.env.MONGO_URL
+			//uri : 'mongodb://localhost:27017/api'
+	    }
+	}
+
